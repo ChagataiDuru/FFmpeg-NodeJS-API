5d2111fb2743350f5797add29b7318192fc19cbf
Çağatay <durucagatay01@gmail.com>
2023-08-03 15:10:33 +0300

    First initializations of Dockerfile and ffmepg setup

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 'use strict';
-var express = require('express');
-var cors = require('cors')
-var ffmpeg = require('fluent-ffmpeg');
-var PORT = 8080;
-var HOST = '0.0.0.0';
-const app = express();
-app.use(cors());
-app.get('/', (req, res) => {
-    res.contentType('audio/mp3');
-    res.attachment('myfile.mp3');
-    var pathToAudio = 'https://dl.dropbox.com/s/pc7qp4wrf46t9op/test-clip.webm?dl=0';
-    ffmpeg(pathToAudio)
-        .toFormat('mp3')
-        .on('end', function(err) {
-            console.log('done!')
-        })
-        .on('error', function(err) {
-            console.log('an error happened: ' + err.message);
-        })
-        .pipe(res, {end: true})
+const Hapi = require('hapi');
+const ffmpeg = require('fluent-ffmpeg');
+
+const server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: 8080
 });
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply.file('myfile.mp3', {
+            confine: false,
+            filename: 'myfile.mp3',
+            mode: 'attachment',
+            path: 'https://dl.dropbox.com/s/pc7qp4wrf46t9op/test-clip.webm?dl=0'
+        }).type('audio/mp3');
+    }
+});
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
