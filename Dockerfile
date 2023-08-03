d326f1463a9b2ce92fc7383d262fc06d43896723
Çağatay <durucagatay01@gmail.com>
2023-08-03 16:45:04 +0300

    Fixing docker bug and Writing neccessery routes

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
-FROM rickydunlop/nodejs-ffmpeg
+FROM node:18
 VOLUME ["/root"]
 ADD ffmpeg-setup.sh /root
 WORKDIR /usr/src/app
 COPY package*.json ./
-RUN npm install hapi
 RUN npm install
 COPY node_modules ./node_modules
 COPY . .
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
-const Hapi = require('hapi');
+const Hapi = require('@hapi/hapi');
+const Vision = require('@hapi/vision');
+const Inert = require('@hapi/inert');
 const Path = require('path');
 const fs = require('fs');
 const Busboy = require('busboy');
@@ -7,14 +9,22 @@ const ffmpeg = require('fluent-ffmpeg');
 
 const server = new Hapi.Server({
     port: 8080,
-    host: 'localhost'
+    host: '0.0.0.0',
+    routes: {
+        payload: {
+            output: 'stream',
+            parse: false,
+            allow: ['multipart/form-data', 'application/octet-stream', 'video/mp4', 'video/quicktime'],
+            maxBytes: 52428800 // 50MB
+        }
+    }
 });
 
 server.route({
     method: 'GET',
     path: '/',
-    handler: function (request, reply) {
-        reply.file('index.html');
+    handler: function (request, reply){
+        return reply.file('public/index.html');
     }
 });
 
@@ -22,19 +32,37 @@ server.route({
     method: 'POST',
     path: '/upload',
     handler: function (request, reply) {
-        const busboy = new Busboy({ headers: request.headers });
+        console.log('Uploading file');
+        const busboy = Busboy({ headers: request.headers });
         const fileData = {};
         busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+            console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
             const saveTo = Path.join(__dirname, 'uploads', filename);
             file.pipe(fs.createWriteStream(saveTo));
             fileData.path = saveTo;
             fileData.type = mimetype;
+            console.log('File saved to: ' + saveTo);
         });
         busboy.on('finish', function() {
+            console.log('Done parsing form!');
             reply(JSON.stringify(fileData));
+            const filePath = fileData.path;
+            const fileType = fileData.type.split('/')[1];
+            const fileName = Path.basename(filePath, Path.extname(filePath));
+            const convertedFilePath = Path.join(__dirname, 'converted', `${fileName}.${fileType}`);
+            ffmpeg(filePath)
+                .toFormat(fileType)
+                .on('end', function() {
+                    console.log('File converted successfully');
+                })
+                .on('error', function(err) {
+                    console.log('An error occurred: ' + err.message);
+                })
+                .save(convertedFilePath);
+            console.log('File converted to: ' + convertedFilePath);
         });
-        request.pipe(busboy);
-    }
+        require('request')(request).pipe(busboy);
+    },
 });
 
 server.route({
@@ -45,23 +73,16 @@ server.route({
         const fileType = request.query.type;
         const fileName = Path.basename(filePath, Path.extname(filePath));
         const convertedFilePath = Path.join(__dirname, 'converted', `${fileName}.${fileType}`);
-        ffmpeg(filePath)
-            .toFormat(fileType)
-            .on('end', function() {
-                reply.file(convertedFilePath, {
-                    confine: false,
-                    filename: `${fileName}.${fileType}`,
-                    mode: 'attachment'
-                }).type(fileType);
-            })
-            .on('error', function(err) {
-                console.log('An error occurred: ' + err.message);
-                reply(err);
-            })
-            .save(convertedFilePath);
+        reply.file(convertedFilePath, {
+            confine: false,
+            filename: `${fileName}.${fileType}`,
+            mode: 'attachment'
+        }).type(fileType);
     }
 });
 
 server.start(function () {
     console.log('Server running at:', server.info.uri);
 });
+
+server.register([Inert.plugin, Vision.plugin]);
