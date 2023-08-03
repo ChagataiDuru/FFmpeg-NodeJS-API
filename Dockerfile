7a7ad66b9491953ca970c9f1b857361037fec215
Çağatay <durucagatay01@gmail.com>
2023-08-03 15:39:21 +0300

    Running server on Docker

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
-FROM node:18
+FROM rickydunlop/nodejs-ffmpeg
 VOLUME ["/root"]
 ADD ffmpeg-setup.sh /root
-RUN /root/ffmpeg-setup.sh
 WORKDIR /usr/src/app
 COPY package*.json ./
+RUN npm install hapi
 RUN npm install
+COPY node_modules ./node_modules
 COPY . .
 EXPOSE 8080
 CMD [ "npm", "start" ]
--- server.js
+++ server.js
@@ -1,23 +1,64 @@
 'use strict';
 const Hapi = require('hapi');
+const Path = require('path');
+const fs = require('fs');
+const Busboy = require('busboy');
 const ffmpeg = require('fluent-ffmpeg');
 
-const server = new Hapi.Server();
-server.connection({
-    host: 'localhost',
-    port: 8080
+const server = new Hapi.Server({
+    port: 8080,
+    host: 'localhost'
 });
 
 server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
-        reply.file('myfile.mp3', {
-            confine: false,
-            filename: 'myfile.mp3',
-            mode: 'attachment',
-            path: 'https://dl.dropbox.com/s/pc7qp4wrf46t9op/test-clip.webm?dl=0'
-        }).type('audio/mp3');
+        reply.file('index.html');
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/upload',
+    handler: function (request, reply) {
+        const busboy = new Busboy({ headers: request.headers });
+        const fileData = {};
+        busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+            const saveTo = Path.join(__dirname, 'uploads', filename);
+            file.pipe(fs.createWriteStream(saveTo));
+            fileData.path = saveTo;
+            fileData.type = mimetype;
+        });
+        busboy.on('finish', function() {
+            reply(JSON.stringify(fileData));
+        });
+        request.pipe(busboy);
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/convert',
+    handler: function (request, reply) {
+        const filePath = request.query.path;
+        const fileType = request.query.type;
+        const fileName = Path.basename(filePath, Path.extname(filePath));
+        const convertedFilePath = Path.join(__dirname, 'converted', `${fileName}.${fileType}`);
+        ffmpeg(filePath)
+            .toFormat(fileType)
+            .on('end', function() {
+                reply.file(convertedFilePath, {
+                    confine: false,
+                    filename: `${fileName}.${fileType}`,
+                    mode: 'attachment'
+                }).type(fileType);
+            })
+            .on('error', function(err) {
+                console.log('An error occurred: ' + err.message);
+                reply(err);
+            })
+            .save(convertedFilePath);
     }
 });
 
